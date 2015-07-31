bd56bec8ee9686c71532014b3a02df06fe612501
DengBin <284825870@qq.com>
2015-07-31 14:44:12 +0800

    login register

---
+++ MyApp/MyApp/LoginAndRegister/DBLoginVC.h
@@ -0,0 +1,13 @@
+//
+//  DBLoginVC.h
+//  MyApp
+//
+//  Created by DengBin on 15/7/31.
+//  Copyright (c) 2015年 DengBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBLoginVC : UIViewController
+
+@end
---
+++ MyApp/MyApp/LoginAndRegister/DBRegisterVC.h
@@ -0,0 +1,13 @@
+//
+//  DBRegisterVC.h
+//  MyApp
+//
+//  Created by DengBin on 15/7/31.
+//  Copyright (c) 2015年 DengBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBRegisterVC : UIViewController
+
+@end
